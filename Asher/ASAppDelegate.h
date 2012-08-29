5578c67dc8396701a0f374dda7c3b1c60dc0e68d
Alan Zeino <alan.zeino@gmail.com>
2012-08-29 17:53:08 +1000

    Initial Commit

---
+++ Asher/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  Asher
+//
+//  Created by Alan Zeino on 29/08/12.
+//  Copyright (c) 2012 Alan Zeino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Asher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Asher
+//
+//  Created by Alan Zeino on 29/08/12.
+//  Copyright (c) 2012 Alan Zeino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
